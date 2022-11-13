9d80dade0349623dd42ae97a0d5a87874122bc01
teatroCamillo <teatrocamillo@gmail.com>
2022-11-13 17:19:28 +0100

    Two indicators for tech analysis RSI & MACD added.

--- server/src/main/java/pdapp/server/controller/TechnicalAnalysisDataController.java
+++ server/src/main/java/pdapp/server/controller/TechnicalAnalysisDataController.java
@@ -1,11 +1,8 @@
 package pdapp.server.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -14,22 +11,12 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.ta4j.core.Bar;
-import org.ta4j.core.BarSeries;
-import org.ta4j.core.Rule;
-import org.ta4j.core.indicators.RSIIndicator;
-import org.ta4j.core.indicators.SMAIndicator;
-import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
-import org.ta4j.core.rules.CrossedDownIndicatorRule;
-import org.ta4j.core.rules.OverIndicatorRule;
+import pdapp.server.model.TechData;
 import pdapp.server.service.TechnicalAnalysisDataService;
 
 import java.io.File;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
-import java.util.stream.Collectors;
-
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -38,7 +25,6 @@ public class TechnicalAnalysisDataController {
 
     private final String  EXCHANGE_RATES_API = "https://currency-conversion-and-exchange-rates.p.rapidapi" +
             ".com/";
-
     private final WebClient webClient;
     private final ObjectMapper om;
     private final TechnicalAnalysisDataService tads;
@@ -51,12 +37,10 @@ public class TechnicalAnalysisDataController {
     }
 
     @GetMapping("/{userId}/tech-analysis")
-    public ResponseEntity<?> getAllQuestions() throws JsonProcessingException {
-        Map<String, String> resutlMap = new HashMap<>();
+    public ResponseEntity<?> getAllQuestions() {
         log.info("Response Client");
         String url = EXCHANGE_RATES_API + "/timeseries?start_date=2022-09-01&end_date=2022-10-31";
 
-        // this "resp" can be return as body for this ResponeEntity as json
 //        HashMap resp = webClient
 //                .get()
 //                .uri(url)
@@ -68,48 +52,15 @@ public class TechnicalAnalysisDataController {
 //                .bodyToMono(HashMap.class)
 //                .block();
 
-
-        Map<String,Object> input = new HashMap<>();
+        List<TechData> input = new ArrayList<>();
         try {
-            Resource resource = new ClassPathResource("/static/response.json");
+            Resource resource = new ClassPathResource("/static/data-eur-usd.json");
             File file = resource.getFile();
-            input = om.readValue(file, new TypeReference<HashMap<String, Object>>() {});
-
-        }catch (Exception e){
+            input = om.readValue(file, new TypeReference<>(){});
+        } catch (Exception e){
             log.info(e.getMessage());
         }
-
-        //input.forEach((k,v) -> log.info(k + " : " + v));
-
-        // prepare data *******************************************************************************************
-
-        // treeMap is auto-sorting data by key
-        TreeMap<LocalDate, String> tm = tads.getSortedRatesMapForEURUSD(input);
-
-        //tm.forEach((k,v) -> log.info(k + " : " + v));
-
-        BarSeries series = tads.createBarSeriesFromRatesMap(tm);
-        int quantity = series.getBarCount();
-        for(int i=0; i<quantity; i++){
-            log.info(series.getBar(i).toString());
-        }
-        // *******************************************************************************************
-
-        // RSI *******************************************************************************************
-        int barCount = series.getBarCount();
-        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
-        RSIIndicator rsi = new RSIIndicator(closePrice, 14);
-
-        for(int i = 0; i < rsi.getBarSeries().getBarCount(); i++){
-            log.info("RSI: " + " index: " + i + " : "  + rsi.getValue(i) + " : " + rsi.getBarSeries().getBar(i));
-        }
-        resutlMap.put("actualRSI", String.valueOf(rsi.getValue(rsi.getBarSeries().getEndIndex())));
-        // *******************************************************************************************
-
-        // 2-period cross RSI ************************************************************************
-        // *******************************************************************************************
-
-        return new ResponseEntity<>(resutlMap, HttpStatus.OK);
+        //input.forEach(e -> log.info(e.toString()));
+        return new ResponseEntity<>(tads.strategy(input), HttpStatus.OK);
     }
-
 }
--- server/src/main/java/pdapp/server/service/TechnicalAnalysisDataService.java
+++ server/src/main/java/pdapp/server/service/TechnicalAnalysisDataService.java
@@ -1,25 +1,23 @@
 package pdapp.server.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeriesBuilder;
+import org.ta4j.core.indicators.MACDIndicator;
+import org.ta4j.core.indicators.RSIIndicator;
+import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
+import pdapp.server.model.TechData;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -31,35 +29,72 @@ public class TechnicalAnalysisDataService {
         this.om = new ObjectMapper();
     }
 
-    public BarSeries createBarSeriesFromRatesMap(TreeMap<LocalDate, String> sortedRatesMap){
+    /**
+     * Assumptions
+     * 1. Game for growth
+     * 2. RSI 14 (daily interval) is lower than or equal 30.
+     * 3. Don't focus on details or single bar
+     * 4. Long-term transaction
+     * 5. MACD below 0 level
+     * 6. Thanks the platform trader doesn't see a chart
+     */
+
+    public Map<String, String> strategy(List<TechData> input){
+        Map<String, String> resultMap = new HashMap<>();
+
+        // prepare data
+        TreeMap<LocalDate, TechData> tm = getSortedRatesMapForEURUSD(input);
+
+        // create barSeries
+        BarSeries series = createBarSeriesFromRatesMap(tm);
+
+        // ClosePriceIndicator
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
+        resultMap.put("closePrice", closePrice.getValue(series.getEndIndex()).toString());
+
+
+        // RSI-14 *******************************************************************************************
+        RSIIndicator rsi = new RSIIndicator(closePrice, 14);
+//        for(int i = 0; i < rsi.getBarSeries().getBarCount(); i++){
+//            log.info("RSI-14: " + " index: " + i + " : "  + rsi.getValue(i) + " : " + rsi.getBarSeries().getBar(i));
+//        }
+        resultMap.put("actualRSI-14", String.valueOf(rsi.getValue(rsi.getBarSeries().getEndIndex())));
+        // *******************************************************************************************
+
+        // MACD ************************************************************************
+        MACDIndicator macd = new MACDIndicator(closePrice);
+        for(int i = 0; i < macd.getBarSeries().getBarCount(); i++){
+            log.info("macd: " + " index: " + i + " : "  + macd.getValue(i) + " : " + macd.getBarSeries().getBar(i));
+        }
+        resultMap.put("macd", String.valueOf(macd.getValue(macd.getBarSeries().getEndIndex())));
+        // ************************************************************************
+
+        return resultMap;
+    }
+
+    private BarSeries createBarSeriesFromRatesMap(TreeMap<LocalDate, TechData> sortedRatesMap){
         BarSeries series = new BaseBarSeriesBuilder().withName("mySeries").build();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
 
-        //ZonedDateTime zdt = LocalDate.parse("2022-04-26", dateTimeFormatter).atStartOfDay(ZoneId.of("UTC"));
-        //log.info("zone date time: " + zdt);
-
-        for(Map.Entry entry : sortedRatesMap.entrySet()){
-            ZonedDateTime zdt = LocalDate.parse(entry.getKey().toString(), dateTimeFormatter).atStartOfDay(ZoneId.of("UTC"));
-            series.addBar(zdt, entry.getValue().toString(), entry.getValue().toString(),entry.getValue().toString(),entry.getValue().toString());
+        for(LocalDate entry : sortedRatesMap.keySet()){
+            ZonedDateTime zdt = LocalDate.parse(entry.toString(), dateTimeFormatter).atStartOfDay(ZoneId.of("UTC"));
+            series.addBar(zdt,
+                    sortedRatesMap.get(entry).getOpen(),
+                    sortedRatesMap.get(entry).getHigh(),
+                    sortedRatesMap.get(entry).getLow(),
+                    sortedRatesMap.get(entry).getPrice());
         }
-
         return series;
     }
 
-    public TreeMap<LocalDate, String> getSortedRatesMapForEURUSD(Map<String,Object> response) throws JsonProcessingException {
-        // take value from map from rates key and by Gson convert to string and then to new Map
-        Gson gson = new Gson();
-        String ent = gson.toJson(response.get("rates"), LinkedHashMap.class);
-        Map<String, Object> rats = om.readValue(ent, new TypeReference<HashMap<String, Object>>() {});
+    private TreeMap<LocalDate, TechData> getSortedRatesMapForEURUSD(List<TechData> response){
 
-        //mapping to date for sorting purpose by date
-        Map<LocalDate, String> newRats = rats.entrySet().stream()
-                .collect(
-                        Collectors.toMap(
-                                e -> LocalDate.parse(e.getKey(), DateTimeFormatter.ISO_LOCAL_DATE),
-                                e -> StringUtils.substringBetween(e.getValue().toString(), "USD=",",")));
+        TreeMap<LocalDate, TechData> rats = new TreeMap<>();
 
-        return new TreeMap<>(newRats);
+        for (TechData techData : response) {
+            rats.put(LocalDate.parse(techData.getDate(), DateTimeFormatter.ofPattern("MM/dd/yyyy")), techData);
+        }
+        return rats;
     }
 
 }

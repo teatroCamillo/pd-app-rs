4dde21ec6bc378a5a2bdf99bdbbe71d4d4701983
teatroCamillo <teatrocamillo@gmail.com>
2023-02-18 21:04:13 +0100

    improved steps page - accordion & alignment

--- pd-ui/src/components/StepsAndResultComponent.jsx
+++ pd-ui/src/components/StepsAndResultComponent.jsx
@@ -3,11 +3,13 @@ import TechnicalAnalysisService from "../api/TechnicalAnalysisService";
 import MacroAnalysisService from "../api/MacroAnalysisService";
 import OutcomeService from "../api/OutcomeService";
 import Util from "../components/utils/Util";
-import { useState } from "react"
+import { useState } from "react";
+import Accordion from 'react-bootstrap/Accordion';
 
 const StepsAndResultComponent = (props) => {
 
     const pair = props.pair;
+
     //prsonal data
     const [personalRespStatus, setPrsonalRespStatus] = useState(false);
     const [gambling, setGambling] = useState('');
@@ -81,83 +83,97 @@ const StepsAndResultComponent = (props) => {
     return (
         <div className="d-flex start-container text-center justify-content-center align-items-center">
             <div className="left-right col-4 mx-2">
-                <div className="start-personal bg-light text-dark rounded-4 mx-2 mb-2">
-                    <h6>Personal Examination</h6>
-                    {personalRespStatus &&
-                        <ul className="text-start">
-                            <li>Risk: {risk}</li>
-                            <li>Gambling: {gambling}</li>
-                        </ul>
-                    }
-                    {!personalRespStatus &&
-                        <button
-                            className="btn btn-primary"
-                            type="button"
-                            onClick={getPersonalResults}
-                        >
-                            Check
-                        </button>
-                    }
-                </div>
-                <div className="start-tech bg-light text-dark rounded-4 mx-2 mb-2">
-                    <h6>Technical Analysis</h6>
-                    {techRespStatus &&
-                        <ul className="text-start">
-                            <li>RSI 14: {rsi14}</li>
-                            <li>MACD: {macd}</li>
-                            <li>Close price: {closePrice}</li>
-                        </ul>
-                    }
-                    {!techRespStatus &&
-                        <button
-                            className="btn btn-primary"
-                            type="button"
-                            onClick={getTechResults}
-                            disabled={!personalRespStatus}
-                        >
-                            Check
-                        </button>
-                    }
-                </div>
-                <div className="start-macro bg-light text-dark rounded-4 mx-2">
-                    <h6>Macrodata Analysis</h6>
-                    {macroRespStatus &&
-                        <ul className="text-start">
-                            <li>GDP growth: {gdpGrowth}</li>
-                            <li>EA inflation: {eaInf}</li>
-                            <li>US inflation: {usInf}</li>
-                        </ul>
-                    }
-                    {!macroRespStatus &&
-                        <button
-                            className="btn btn-primary"
-                            type="button"
-                            onClick={getMacroResults}
-                            disabled={!techRespStatus}
-                        >
-                            Check
-                        </button>
-                    }
-                </div>
+                <Accordion alwaysOpen>
+                    <Accordion.Item eventKey="0" className="bg-light text-dark mx-2 mb-2">
+                        <Accordion.Header><h6>Personal Examination</h6></Accordion.Header>
+                            <Accordion.Body>
+                                {personalRespStatus &&
+                                    <ul className="text-start">
+                                        <li>Risk: {risk}</li>
+                                        <li>Gambling: {gambling}</li>
+                                    </ul>
+                                }
+                                {!personalRespStatus &&
+                                    <button
+                                        className="btn btn-primary"
+                                        type="button"
+                                        onClick={getPersonalResults}
+                                    >
+                                        Check
+                                    </button>
+                                }
+                            </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1" className="bg-light text-dark mx-2 mb-2">
+                        <Accordion.Header><h6>Technical Analysis</h6></Accordion.Header>
+                        <Accordion.Body>
+                            {techRespStatus &&
+                                <ul className="text-start">
+                                    <li>RSI 14: {rsi14}</li>
+                                    <li>MACD: {macd}</li>
+                                    <li>Close price: {closePrice}</li>
+                                </ul>
+                            }
+                            {!techRespStatus &&
+                                <button
+                                    className="btn btn-primary"
+                                    type="button"
+                                    onClick={getTechResults}
+                                    disabled={!personalRespStatus}
+                                >
+                                    Check
+                                </button>
+                            }
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2" className="bg-light text-dark mx-2 mb-2">
+                        <Accordion.Header><h6>Macrodata Analysis</h6></Accordion.Header>
+                        <Accordion.Body>
+                            {macroRespStatus &&
+                                <ul className="text-start">
+                                    <li>GDP growth: {gdpGrowth}</li>
+                                    <li>EA inflation: {eaInf}</li>
+                                    <li>US inflation: {usInf}</li>
+                                </ul>
+                            }
+                            {!macroRespStatus &&
+                                <button
+                                    className="btn btn-primary"
+                                    type="button"
+                                    onClick={getMacroResults}
+                                    disabled={!techRespStatus}
+                                >
+                                    Check
+                                </button>
+                            }
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
             </div>
-            <div className="left-right col-5 bg-light text-dark rounded-4 mx-2">
-                <h2>Outcome for {pair}</h2>
+            <div className="d-flex flex-column left-right col-5 bg-light text-dark rounded-4 mx-2">
+                <div className="d-flex justify-content-center mt-2">
+                    <h2>Outcome for {pair}</h2>
+                </div>
                 {outcomeResp &&
-                    <div>
-                        <h4>Regard to actual data and your predispositions.</h4>
-                        <h4>The chances of success are around</h4>
+                    <div className="mt-3">
+                        <h5>Regard to actual data and your predispositions,</h5>
+                        <h5>the chances of success are around</h5>
                         <h1 style={{color: Util.setColorForOutcomeScore(score)}} >{score}%</h1>
                     </div>
                 }
                 {!outcomeResp &&
-                    <button
-                        className="btn btn-primary"
-                        type="button"
-                        onClick={getOutcome}
-                        disabled={!macroRespStatus}
-                    >
-                        Recap
-                    </button>
+                    <div className="d-flex flex-column justify-content-center align-items-center flex-grow-1">
+                        <h6>First of all, gather data and then recap</h6>
+                        <span className="material-symbols-outlined">query_stats</span>
+                        <button
+                            className="btn btn-primary mt-3"
+                            type="button"
+                            onClick={getOutcome}
+                            disabled={!macroRespStatus}
+                        >
+                            Recap
+                        </button>
+                    </div>
                 }
             </div>
         </div>

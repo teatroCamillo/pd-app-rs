9c114467f4f314970c8f8cdbe34c402d34424599
teatroCamillo <teatrocamillo@gmail.com>
2023-03-08 12:33:03 +0100

    clean code & added Constant.js file

---
+++ pd-ui/src/api/Constant.js
@@ -0,0 +1,6 @@
+
+class Constant {
+    API_URL = 'http://localhost:8080'
+}
+
+export default new Constant();
--- pd-ui/src/components/alerts/DifferentPasswords.jsx
+++ pd-ui/src/components/alerts/DifferentPasswords.jsx
@@ -1,8 +1,8 @@
 import Alert from 'react-bootstrap/Alert';
 
-const DifferentPasswords = ({showDifferentPasswords, setDifferentPasswords}) => {
+const DifferentPasswords = ({differentPasswords, setDifferentPasswords}) => {
 
-  if(showDifferentPasswords){
+  if(differentPasswords){
     return (
       <Alert className='w-100' variant="danger" onClose={() => setDifferentPasswords(false)} dismissible>
         <h6>Oh snap! We've detected different passwords in the registration form. Please check it and try again.</h6>

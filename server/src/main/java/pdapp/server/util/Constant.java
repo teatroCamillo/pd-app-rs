f0031e3fa1c1774fb8db9214bd77f153fee9a09d
teatroCamillo <teatrocamillo@gmail.com>
2023-03-15 16:51:37 +0100

    clean up

--- server/src/main/java/pdapp/server/util/Constant.java
+++ server/src/main/java/pdapp/server/util/Constant.java
@@ -11,20 +11,18 @@ public class Constant {
      * moderately addicted: at least 4 and less than 7
      * not addicted: less than 4
      */
-
     public final static String HIGHLY_ADDICTED = "highly addicted";
     public final static String MODERATELY_ADDICTED = "moderately addicted";
     public final static String NOT_ADDICTED = "not addicted";
 
     /**
-     * The score that you receive on the Investment Risk Test can be interpreted as follows:
+     * The score that is received on the Investment Risk Test can be interpreted as follows:
      * 18 or below = Low risk tolerance (i.e., conservative investor)
      * 19 to 22 = Below-average risk tolerance
      * 23 to 28 = Average/moderate risk tolerance
      * 29 to 32 = Above-average risk tolerance
      * 33 and above = High risk tolerance (i.e., aggressive investor)
      */
-
     public final static String LOW_RISK_TOLERANCE = "low";
     public final static String BELOW_AVERAGE_RISK_TOLERANCE = "below average";
     public final static String AVERAGE_RISK_TOLERANCE = "average";
